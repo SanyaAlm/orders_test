81135d95f4dcf09d9961a19a17db45571d413cdd
Alexander <sasha.14999933@gmail.com>
2025-03-06 12:48:41 +0500

    Add db connection

---
+++ app/infrastructure/db_connection.py
@@ -0,0 +1,16 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from app.infrastructure.config import settings
+
+DATABASE_URL = f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+
+engine = create_async_engine(DATABASE_URL)
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
