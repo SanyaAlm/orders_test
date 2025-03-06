67f680cf465d1127e7cbdc3e08701f3391289312
Alexander <sasha.14999933@gmail.com>
2025-03-06 17:50:07 +0500

    add get user db in user model

--- app/domain/models/user.py
+++ app/domain/models/user.py
@@ -1,7 +1,14 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base
+from ...infrastructure.db_connection import get_async_session
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     pass
+
+    @classmethod
+    def get_user_db(cls, session: AsyncSession = Depends(get_async_session)):
+        return SQLAlchemyUserDatabase(session, User)
