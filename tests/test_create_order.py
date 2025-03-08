4cc4ca1581abb1f9ee46c89985691c5aa1271a88
Alexander <sasha.14999933@gmail.com>
2025-03-08 09:37:13 +0500

    test: add order create tests

---
+++ tests/test_create_order.py
@@ -0,0 +1,39 @@
+async def test_create_order(login_user):
+    """Тест на создание нового заказа."""
+    order_data = {
+        "customer_name": "John Doe",
+        "status": "pending",
+        "products": [
+            {"name": "Laptop", "price": 1000, "quantity": 1},
+            {"name": "Mouse", "price": 50, "quantity": 2},
+        ],
+    }
+
+    async_client, user = login_user
+    response = await async_client.post("/orders/create", json=order_data)
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["customer_name"] == "John Doe"
+    assert data["status"] == "pending"
+    assert data["total_price"] == 1100
+    assert len(data["products"]) == 2
+
+
+async def test_create_wrong_order(login_user):
+    """Тест на создание нового заказа с ошибкой"""
+    order_data = {
+        "customer_name": "John Doe",
+        "status": "new",
+        "products": [
+            {"name": "Laptop", "price": 1000, "quantity": 1},
+            {"name": "Mouse", "price": 50, "quantity": 2},
+        ],
+    }
+
+    async_client, user = login_user
+    response = await async_client.post("/orders/create", json=order_data)
+
+    assert response.status_code == 422
+    data = response.json()
+    assert "detail" in data
